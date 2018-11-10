09734fff8054261a17853fb8ba06fd834b6835a6
Segun Ojo <theuncleshege@gmail.com>
2018-11-10 12:56:59 +0100

    Check if seenIndexes state is an array

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    return (Array.isArray(this.state.seenIndexes.length)) ? this.state.seenIndexes.map(({ number }) => number).join(', ') : '';
   }
 
   renderValues() {
