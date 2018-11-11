550e09c20fd23ab6af02ce4d7914e5a13244ab88
Segun Ojo <theuncleshege@gmail.com>
2018-11-11 17:39:11 +0100

    Fixed container names in deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,15 @@
 kubectl apply -f k8s
 
 kubectl set image deployment/client-deployment client=theuncleshege/multi-docker-client:$SHA
-kubectl set image deployment/api-deployment client=theuncleshege/multi-docker-api:$SHA
-kubectl set image deployment/worker-deployment client=theuncleshege/multi-docker-worker:$SHA
+kubectl set image deployment/api-deployment api=theuncleshege/multi-docker-api:$SHA
+kubectl set image deployment/worker-deployment worker=theuncleshege/multi-docker-worker:$SHA
 
 
 # Other Userful Commands
+# docker run -it -v "$(pwd)":/usr/src/app ruby:2.3 bash
+# gem install travis --no-rdoc --no-ri
+# travis login
+# travis encrypt-file service-account.json -r theuncleshege/multi-docker-k8s
 # kubectl set image deployment/client-deployment client=theuncleshege/multi-docker-client
 # kubectl create secret generic pgpassword --from-literal PGPASSWORD=postgres_password
 # kubectl create serviceaccount --namespace kube-system tiller
